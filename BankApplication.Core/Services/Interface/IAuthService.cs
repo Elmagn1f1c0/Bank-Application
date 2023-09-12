681553fd15dc48b6493cb40703f396ea9197f422
Chibueze Amadi <123497071+Elmagn1f1c0@users.noreply.github.com>
2023-09-12 23:40:31 +0100

    Login and Registration

---
+++ BankApplication.Core/Services/Interface/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using BankApplication.Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication.Core.Services.Interface
+{
+    public interface IAuthService
+    {
+        string GenerateTokenString(LoginUser user);
+        //Task<bool> RegisterUser(RegisterUser user);
+        Task<ResponseDto<bool>> RegisterUser(RegisterUser user);
+        //Task<bool> Login(LoginUser user);
+        Task<ResponseDto<bool>> Login(LoginUser user);
+    }
+}
